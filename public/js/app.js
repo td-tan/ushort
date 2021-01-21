5a37a2a167a4d7fafc5bb65c28cb85ab41ebf67f
td-tan <47648230+3scalate@users.noreply.github.com>
2021-01-21 16:18:16 +0100

    Implement error notice

--- public/js/app.js
+++ public/js/app.js
@@ -19,11 +19,26 @@ window.addEventListener('load', function () {
 
                     const data = login(username, password);
 
-                    form[3].children[0].removeAttribute('hidden');
+                    form[3].children[0].removeAttribute('hidden'); // spinner visible
                     data.then(json => {
-                        console.log(json)
-                        form[3].children[0].setAttribute('hidden', '');
+                        // First check status message
+                        if(json.message === 'failure')
+                        {
+                            // Create error notice on the fly
+                            let error_node = document.createElement('div')
+                            error_node.setAttribute('class', 'mt-3 alert alert-danger');
+                            error_node.setAttribute('role', 'alert');
+                            error_node.textContent = json.body.error_msg;
+
+                            form.appendChild(error_node);
+                            form.classList.remove('was-validated');
+
+                            console.log(form);
+                            console.log(json.body.error_msg);
+                        }
+                        form[3].children[0].setAttribute('hidden', ''); // spinner hidden again
                     });
+                    data.catch(error => console.log(error));
                 }
             }
         });
