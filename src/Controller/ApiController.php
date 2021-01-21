0d2a74ae17087f332817300f413c422a476a8c08
td-tan <47648230+3scalate@users.noreply.github.com>
2021-01-21 15:39:12 +0100

    Implement orm query for email

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -3,12 +3,46 @@
 namespace App\Controller;
 
 use Helper\RequestData;
+use Firebase\JWT\JWT;
+use App\Model\User;
 
 class ApiController
 {
     public function login(RequestData $rd)
     {
-        sleep(3);
-        echo json_encode(array("message" => "success"));
+
+        $user = User::query()->where('email', '=', $rd->body['username']);
+        if($user->count() < 1)
+        {
+            $response = [
+                'message' => 'Failure',
+                'body' => [
+                    'error' => 'User does not exists.'
+                ]
+            ];
+            return json_encode($response);
+        }
+
+        $user = $user->first();
+
+        $payload = array(
+            "iss" => "http://ushort.test",
+            "aud" => "http://ushort.test",
+            "sub" => $user->id,
+            "name" => $user->email,
+            "iat" => time(),
+            "nbf" => time(),
+            "exp" => time() + 60
+        );
+
+        $jwt = JWT::encode($payload, $_ENV['APP_KEY']);
+
+        $response = [
+            'message' => 'Success',
+            'body' => [
+                'access_token' => $jwt
+            ]
+        ];
+        return json_encode($response);
     }
 }
